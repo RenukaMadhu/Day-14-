f65e64dc52865e234a8b9d40896ddb9a074ea859
RenukaMadhu <renuka.cseds25@cmrit.ac.in>
2026-03-05 22:48:18 +0530

    Add files via upload

---
+++ practice6.c
@@ -0,0 +1,11 @@
+// Program to check if the no is divisible by 2 ,0-odd,1-even
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x;
+    printf("enter a number");
+    scanf("%d",&x);
+    printf("%d",x%2==0);
+    return 0;
+}
